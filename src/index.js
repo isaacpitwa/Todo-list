8a37e9fcad40b040f6038a113d631a89b7716a4b
Isaac Pitwa Nyonyintono <31113416+isaacpitwa@users.noreply.github.com>
2022-03-23 15:01:24 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+function component() {
+    const element = document.createElement('div');
+  
+    element.innerHTML = "Hello Webpack";
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+  

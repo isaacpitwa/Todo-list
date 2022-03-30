e7cb1edd3269faaa5d735f546d319801b6c252a1
Alexr16 <j.alex-1996@hotmail.com>
2022-03-30 15:57:57 -0500

    Set functions.test.js

---
+++ src/funtions.test.js
@@ -0,0 +1,24 @@
+import {
+  addTodo, displayTodos, getData, getIsEditing, saveEdit, clearCompleted,
+} from './functions.js';
+
+
+
+describe('Add function', () => {
+  
+  test('returns', () => {
+    expect().toBe();
+  });
+});
+
+describe('Remove function', () => {
+  test('Add one new item to the list', () => {
+    document.body.innerHTML = `
+      <div>
+       <ul id="list"></li>
+        </div>`;
+    addTodo();
+    const list = document.querySelectorAll('#list li');
+    expect(list).toHaveLength(1);
+  });
+});

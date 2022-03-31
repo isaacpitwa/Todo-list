86f7d45d3b2ee08433f272b4bc04a884fb0f5249
Alexr16 <j.alex-1996@hotmail.com>
2022-03-30 20:41:34 -0500

    Add remove test

--- src/funtions.test.js
+++ src/funtions.test.js
@@ -1,14 +1,48 @@
 import {
-  addTodo,
+  addTodo, removeTodo,
 } from './functions.js';
 
-describe('Todo functionality Tests', () => {
-  test('Add one new item to the list', () => {
-    const listBeforeAdd = document.querySelectorAll('#todos-list li');
-    const desc = document.querySelector('#add-todo');
+const fs = require('fs');
+
+describe('Add Todo functionality Tests', () => {
+  beforeAll(() => {
+    document.body.innerHTML = fs.readFileSync('dist/index.html');
+    const desc = document.getElementById('add-todo');
     desc.value = 'Test Description';
     addTodo();
-    const listAfterAdd = document.querySelectorAll('#todos-list li');
-    expect(listAfterAdd).toHaveLength((listBeforeAdd.length + 1));
+  });
+  test('Add one new item to the list', () => {
+    const listAfterAdd = document.querySelectorAll('#todos-list');
+    expect(listAfterAdd).toHaveLength(1);
+  });
+  test('Todos is define', () => {
+    expect(localStorage.todos).toBeDefined();
+  });
+  test('Todos is not null', () => {
+    expect(localStorage.todos).not.toBeNull();
+  });
+  test('Add one new item to the array', () => {
+    const arr = localStorage.todos;
+    expect(arr.length).toBe(64);
+  });
+});
+
+describe('Remove Todo functionality Tests', () => {
+  beforeAll(() => {
+    document.body.innerHTML = fs.readFileSync('dist/index.html');
+    const desc = document.getElementById('add-todo');
+    desc.value = 'Test Description';
+  });
+  beforeEach(() => {
+    addTodo();
+    removeTodo(1);
+  });
+  test('Remove one item to the list', () => {
+    const listAfterAdd = document.querySelectorAll('.todo');
+    expect(listAfterAdd).toHaveLength(1);
+  });
+  test('Remove one item to the array', () => {
+    const arr = localStorage.todos;
+    expect(arr.length).toBe(2);
   });
 });
